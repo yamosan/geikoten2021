f93e554c3962f73db608575fe774fc205d0fd5cf
yamori-masato <yamorimasato@gmail.com>
2021-09-27 01:55:06 +0900

    設定を追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          10: "#D3D977",
+          20: "#BDCF72",
+          30: "#A7C46C",
+          40: "#91BA67",
+          50: "#7CB061",
+          60: "#66A65C",
+          70: "#509B56",
+          80: "#3A9151",
+        },
+        sky: "#DAF6FF",
+        brown: "#353333",
+        lightBrown: "#504746",
+        gray: "#BFBFBF",
+        lightGray: "#F2F2F2",
+        white: "#FFFFFF",
+
+        menuBg: "rgba(80, 71, 70, 0.9)",
+        // aliases
+        bg: "$lightGray",
+        text: "$lightBrown",
+      },
+      fontSize: {
+        xxs: ".625rem", // 10px
+      },
+      fontFamily: {
+        sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
+        genuine: ["Genuine", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
