46a9ee732acc2395746c69872f2d06481c3f06fb
yamori-masato <yamorimasato@gmail.com>
2021-09-27 01:04:14 +0900

    create-next-app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app";
+
+const App = (props: AppProps) => {
+  return <props.Component {...props.pageProps} />;
+};
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Root: NextPage = () => {
+  return (
+    <>
+      <h1>Hello Next.js!</h1>
+    </>
+  );
+};
+
+export default Root;
