a3b26017d7d54a370198282bfc73183950e5dea9
yamori-masato <yamorimasato@gmail.com>
2021-09-27 21:19:10 +0900

    Imageコンポーネント内でutils関数で使用

--- src/components/basics/Image.tsx
+++ src/components/basics/Image.tsx
@@ -1,13 +1,13 @@
 import NextImage from "next/image";
 import type { ComponentPropsWithoutRef } from "react";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH;
+import { resolvePath } from "@/utils/resolvePath";
 
 // src: string | StaticImport -> string
 type Props = ComponentPropsWithoutRef<typeof NextImage> & { src: string };
 
 const Image = (props: Props) => {
-  const url = props.src?.startsWith("/") ? `${basePath || ""}${props.src}` : props.src;
+  const url = props.src?.startsWith("/") ? resolvePath(props.src) : props.src;
   return <NextImage {...props} src={url} />;
 };
 
