f72d322fbb5a41e2ca6398a7c8f0b509c40cc38d
yamori-masato <yamorimasato@gmail.com>
2021-09-27 21:13:24 +0900

    url定数を追加

---
+++ src/constants/urls.ts
@@ -0,0 +1,5 @@
+// SNS urls
+export const TWITTER_URL = "https://twitter.com/geik0";
+export const INSTAGRAM_URL = "https://instagram.com/geikoten2021";
+
+export const HOMEPAGE_LINK = "https://www.nagoya-cu.ac.jp/sda";
