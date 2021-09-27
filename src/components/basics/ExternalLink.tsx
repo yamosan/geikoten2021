6f7be35555bf4810114a0cfff4cc92e15d35ab73
yamori-masato <yamorimasato@gmail.com>
2021-09-27 21:16:57 +0900

    ExternalLinkコンポーネントを追加

---
+++ src/components/basics/ExternalLink.tsx
@@ -0,0 +1,15 @@
+import type { ComponentPropsWithoutRef } from "react";
+import React, { forwardRef } from "react";
+
+type Props = ComponentPropsWithoutRef<"a"> & { href: string };
+
+export const ExternalLink = forwardRef<HTMLAnchorElement, Props>((props, ref) => {
+  const { children, ...attrs } = props;
+  return (
+    <a target="_blank" rel="noopener noreferrer" {...attrs} ref={ref}>
+      {children}
+    </a>
+  );
+});
+
+ExternalLink.displayName = "ExternalLink";
