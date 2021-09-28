4a65cccf16323e91f3bd5ad3e28259cebac7bb60
yamori-masato <yamorimasato@gmail.com>
2021-09-29 02:11:18 +0900

    Footerのスタイルを微調整

--- src/components/layouts/PCLayout/Footer.tsx
+++ src/components/layouts/PCLayout/Footer.tsx
@@ -19,14 +19,14 @@ export const Footer: VFC = () => {
   return (
     <footer className="relative bg-lightBrown" style={{ paddingLeft: SIDE_NAV_WIDTH }}>
       {/* TODO: ページ最上部にスクロールするボタン */}
-      <div className="flex items-end justify-between w-full px-4 py-8 text-white lg:px-9">
+      <div className="flex items-end justify-between w-full px-4 py-16 text-white lg:px-9">
         <div className="flex flex-col w-full space-y-5">
           <div className="flex items-end space-x-3 lg:space-x-5">
-            <p className="text-2xl font-bold leading-none">名古屋市立大学芸術工学部</p>
+            <p className="text-xl font-bold leading-none">名古屋市立大学芸術工学部</p>
             <HomepageLink />
           </div>
           <div className="flex items-end justify-between w-full">
-            <p className="text-sm">
+            <p className="text-sm leading-loose">
               〒464-008 名古屋市千種区北千種2丁目1番10号
               <br />
               TEL052-721-1225(代) FAX052-721-3110
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 
+import { Layout } from "@/components/layouts/Layout";
+
 const Root: NextPage = () => {
   return (
-    <>
-      <h1 className="font-genuine">Hello Next.js!</h1>
-    </>
+    <Layout>
+      <h1>Hello Next.js!</h1>
+    </Layout>
   );
 };
 
