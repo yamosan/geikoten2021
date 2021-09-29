d1455199c5fdf60a736f3e625d8ce58eab4158b0
yamori-masato <yamorimasato@gmail.com>
2021-09-30 03:34:19 +0900

    ScrollDownコンポーネントを追加

---
+++ src/components/ScrollDown.tsx
@@ -0,0 +1,43 @@
+import type { VFC } from "react";
+
+export const ScrollDown: VFC = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center space-y-2">
+        <p className="text-xs font-black tracking-widest text-white">scroll</p>
+        <div className="bar"></div>
+      </div>
+
+      <style jsx>{`
+        .bar {
+          @apply relative w-0.5 h-8 rounded-t-full overflow-hidden;
+        }
+
+        .bar::before {
+          @apply block absolute h-8 w-0.5 bg-white opacity-30 rounded-t-full;
+          content: "";
+        }
+
+        .bar::after {
+          @apply block absolute h-16 w-0.5 bg-white rounded-full;
+          content: "";
+          animation: fade-in-down 4s cubic-bezier(0.75, 0, 0.25, 1) infinite;
+        }
+
+        @keyframes fade-in-down {
+          0%,
+          10% {
+            transform: translateY(-100%);
+          }
+          50% {
+            transform: translateY(0%);
+          }
+          90%,
+          100% {
+            transform: translateY(100%);
+          }
+        }
+      `}</style>
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Div100vh from "react-div-100vh";
 import Image from "@/components/basics/Image";
 import { HEADER_HEIGHT } from "@/components/layouts/constants";
 import { Layout } from "@/components/layouts/Layout";
+import { ScrollDown } from "@/components/ScrollDown";
 import { VisitorCounter } from "@/components/VisitorCounter";
 import useMedia from "@/hooks/useMediaQuery";
 
@@ -16,6 +17,7 @@ const Root: NextPage = () => {
         <Image src="/images/top_bg.jpg" alt="背景" layout="fill" objectFit="cover" />
 
         <div className="relative w-full h-full" style={{ paddingTop: HEADER_HEIGHT }}>
+          {/* アクセスカウンタ */}
           {isWide ? (
             <div className="flex justify-end pr-4">
               <VisitorCounter count={1234} size="lg" />
@@ -25,6 +27,11 @@ const Root: NextPage = () => {
               <VisitorCounter count={1234} size="sm" />
             </div>
           )}
+
+          {/* スクロールダウン */}
+          <div className="absolute bottom-0 transform -translate-x-1/2 left-1/2">
+            <ScrollDown />
+          </div>
         </div>
       </Div100vh>
     </Layout>
