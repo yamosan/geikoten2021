674f225ab40080e13bab2d2826bbc807072ba6ed
yamori-masato <yamorimasato@gmail.com>
2021-09-29 11:43:44 +0900

    themeを修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,36 +5,41 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false,
   theme: {
-    extend: {
-      colors: {
-        green: {
-          10: "#D3D977",
-          20: "#BDCF72",
-          30: "#A7C46C",
-          40: "#91BA67",
-          50: "#7CB061",
-          60: "#66A65C",
-          70: "#509B56",
-          80: "#3A9151",
-        },
-        sky: "#DAF6FF",
-        brown: "#353333",
-        lightBrown: "#504746",
-        gray: "#BFBFBF",
-        lightGray: "#F2F2F2",
-        white: "#FFFFFF",
-
-        menuBg: "rgba(80, 71, 70, 0.9)",
-        // aliases
-        bg: "#F2F2F2", // lightGray
-        text: "#504746", // lightBrown
+    colors: {
+      green: {
+        10: "#D3D977",
+        20: "#BDCF72",
+        30: "#A7C46C",
+        40: "#91BA67",
+        50: "#7CB061",
+        60: "#66A65C",
+        70: "#509B56",
+        80: "#3A9151",
       },
+      sky: "#DAF6FF",
+      brown: "#353333",
+      lightBrown: "#504746",
+      gray: "#BFBFBF",
+      lightGray: "#F2F2F2",
+      white: "#FFFFFF",
+
+      menuBg: "rgba(80, 71, 70, 0.9)",
+      // aliases
+      bg: "#F2F2F2", // lightGray
+      text: "#504746", // lightBrown
+    },
+    fontFamily: {
+      sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
+      genuine: ["Genuine", ...defaultTheme.fontFamily.sans],
+    },
+    fontWeight: {
+      medium: "500",
+      bold: "700",
+      black: "900",
+    },
+    extend: {
       fontSize: {
         xxs: ".625rem", // 10px
-      },
-      fontFamily: {
-        sans: ["Noto Sans JP", ...defaultTheme.fontFamily.sans],
-        genuine: ["Genuine", ...defaultTheme.fontFamily.sans],
       },
     },
   },
