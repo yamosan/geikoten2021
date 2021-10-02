dc7a1358a273e41cec627fa8878084aac7634b42
yamori-masato <yamorimasato@gmail.com>
2021-10-03 02:11:50 +0900

    アクセシビリティ改善

--- src/components/layouts/SPLayout/HamburgerMenu.tsx
+++ src/components/layouts/SPLayout/HamburgerMenu.tsx
@@ -12,37 +12,40 @@ export const HamburgerMenu: VFC<Props> = ({ onClick, isOpen, color, transition }
   return (
     <button
       onClick={onClick}
-      className={clsx(" w-10 h-10 relative focus:outline-none", {
+      className={clsx(" w-10 h-10 relative", {
         "text-white": color === "white",
         "text-brown": color === "black",
       })}
     >
-      <div className="absolute left-1/2 top-3.5 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="block w-5">
-          <div
-            aria-hidden="true"
-            className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
-              "rotate-45": isOpen,
-              "translate-y-1.5": !isOpen,
-            })}
-          ></div>
-          <div
-            aria-hidden="true"
-            className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
-              "opacity-0": isOpen,
-            })}
-          ></div>
-          <div
-            aria-hidden="true"
-            className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
-              "-rotate-45": isOpen,
-              "-translate-y-1.5": !isOpen,
-            })}
-          ></div>
+      <span className="sr-only">Close</span>
+      <div aria-hidden={true}>
+        <div className="absolute left-1/2 top-3.5 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="block w-5">
+            <div
+              aria-hidden="true"
+              className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
+                "rotate-45": isOpen,
+                "translate-y-1.5": !isOpen,
+              })}
+            ></div>
+            <div
+              aria-hidden="true"
+              className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
+                "opacity-0": isOpen,
+              })}
+            ></div>
+            <div
+              aria-hidden="true"
+              className={clsx("absolute h-0.5 w-5 rounded bg-current transform", transition, {
+                "-rotate-45": isOpen,
+                "-translate-y-1.5": !isOpen,
+              })}
+            ></div>
+          </div>
         </div>
-      </div>
-      <div className={clsx("absolute bottom-1 left-1/2 -translate-x-1/2 transform", transition)}>
-        <p className="font-bold leading-none text-xxs">{isOpen ? "CLOSE" : "OPEN"}</p>
+        <div className={clsx("absolute bottom-1 left-1/2 -translate-x-1/2 transform", transition)}>
+          <p className="font-bold leading-none text-xxs">{isOpen ? "CLOSE" : "OPEN"}</p>
+        </div>
       </div>
     </button>
   );
