505dc37ef637406726c9bcc74f1b8845f843e854
yamori-masato <yamorimasato@gmail.com>
2021-10-02 22:39:13 +0900

    useWindowSize hooksを追加

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const isBrowser = typeof window !== "undefined";
+
+const useWindowSize = () => {
+  const [state, setState] = useState<{ width: number; height: number }>({
+    width: isBrowser ? innerWidth : Infinity,
+    height: isBrowser ? innerHeight : Infinity,
+  });
+
+  useEffect(() => {
+    const onResize = () => {
+      setState({
+        width: innerWidth,
+        height: innerHeight,
+      });
+    };
+    onResize();
+
+    // debounce
+    let timeoutId: NodeJS.Timeout | undefined = undefined;
+    const onResizeDebounce = () => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(onResize, 200);
+    };
+
+    window.addEventListener("resize", onResizeDebounce);
+    return () => {
+      window.removeEventListener("resize", onResizeDebounce);
+      if (timeoutId) clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return state;
+};
+
+export default useWindowSize;
