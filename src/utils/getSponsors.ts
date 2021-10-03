111c71f5fa1b93b77f6c572c01a8d357f744217d
yamori-masato <yamorimasato@gmail.com>
2021-10-04 01:06:03 +0900

    引数queryを汎用化

--- src/utils/getSponsors.ts
+++ src/utils/getSponsors.ts
@@ -4,12 +4,15 @@ import type { Sponsor } from "@/models/Sponsor";
 const SPONSORS = sponsors as Sponsor[];
 
 type Query = {
-  rank: Sponsor["rank"][];
+  rank: Sponsor["rank"] | Sponsor["rank"][];
 };
 
 export function getSponsors(query?: Query): Sponsor[] {
   if (!query) return SPONSORS;
   const { rank } = query;
 
+  if (typeof rank === "string") {
+    return SPONSORS.filter((sponsor) => sponsor.rank === rank);
+  }
   return SPONSORS.filter((sponsor) => rank.includes(sponsor.rank));
 }
