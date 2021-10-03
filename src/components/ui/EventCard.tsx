a07dd6ebe9fe0c001f3cbd5ddf482626f3009ba2
yamori-masato <yamorimasato@gmail.com>
2021-10-04 03:39:58 +0900

    ホバーエフェクトを追加

--- src/components/ui/EventCard.tsx
+++ src/components/ui/EventCard.tsx
@@ -14,12 +14,12 @@ export const EventCard = forwardRef<HTMLDivElement, Props>((props, ref) => {
   const { title, description, imageSrc, className, ...attrs } = props;
 
   return (
-    <div className={clsx("relative flex flex-col space-y-2 bg-white p-6", className)} {...attrs} ref={ref}>
+    <div className={clsx("relative flex flex-col space-y-2 bg-white p-6 group", className)} {...attrs} ref={ref}>
       <div className="flex items-end justify-between">
         <div className="text-2xl font-bold text-text">{title}</div>
         <div className="text-text">{description}</div>
       </div>
-      <figure className="relative h-[236px] w-[420px] flex-shrink-0">
+      <figure className="relative h-[236px] w-[420px] flex-shrink-0 transform duration-300 group-hover:opacity-70">
         <Image src={imageSrc} alt={title} layout="fill" />
       </figure>
     </div>
