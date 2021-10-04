6cac6e6b303ec70fa8a36fc7f520cc38202a7c63
yamori-masato <yamorimasato@gmail.com>
2021-10-04 23:47:29 +0900

    スムーススクロールのポリフィルを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "tailwindcss/tailwind.css";
 
 import type { AppProps } from "next/app";
+import smoothscroll from "smoothscroll-polyfill";
+
+smoothscroll.polyfill();
 
 const App = (props: AppProps) => {
   return <props.Component {...props.pageProps} />;
