a08e4c5754392098bbcfa0b1dc0e1c31166eb1fe
yamori-masato <yamorimasato@gmail.com>
2021-10-05 00:47:56 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,9 @@ import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import smoothscroll from "smoothscroll-polyfill";
 
-smoothscroll.polyfill();
+if (process.browser) {
+  smoothscroll.polyfill();
+}
 
 const App = (props: AppProps) => {
   return <props.Component {...props.pageProps} />;
