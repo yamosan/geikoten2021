042f5aacb8e48340319c50ad66cc5552cee2728e
yamori-masato <yamorimasato@gmail.com>
2021-10-06 21:39:07 +0900

    ポリフィルをimport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import "tailwindcss/tailwind.css";
 
+// import "scroll-behavior-polyfill";
 import type { AppProps } from "next/app";
-import smoothscroll from "smoothscroll-polyfill";
-
-if (process.browser) {
-  smoothscroll.polyfill();
-}
 
 const App = (props: AppProps) => {
   return <props.Component {...props.pageProps} />;
