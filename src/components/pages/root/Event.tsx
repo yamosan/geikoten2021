dad1d76f6cbbe56f6132475b1037534edf8aa2b2
yamori-masato <yamorimasato@gmail.com>
2021-10-07 01:55:03 +0900

    水平パララックスをやめる

--- src/components/pages/root/Event.tsx
+++ src/components/pages/root/Event.tsx
@@ -1,45 +1,11 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import type { VFC } from "react";
-import { useEffect, useRef } from "react";
 
 import { Paragraph } from "@/components/basics";
 import { EventCard, Section } from "@/components/ui";
-import useWindowSize from "@/hooks/useWindowSize";
 
 export const Event: VFC = () => {
-  const triggerRef = useRef<HTMLElement>(null);
-  const boxRef = useRef<HTMLDivElement>(null);
-  const anime = useRef<gsap.core.Tween>(null);
-
-  const { width } = useWindowSize();
-  //TODO: リサイズした時にxを変更したい
-  useEffect(() => {
-    const boxElm = boxRef.current;
-    const triggerElm = triggerRef.current;
-    if (boxElm === null || triggerElm === null) return;
-
-    if (anime.current === null) {
-      gsap.registerPlugin(ScrollTrigger);
-      anime.current = gsap.to(boxElm, {
-        // xPercent: -100,
-        x: () => -boxElm.clientWidth + width * 0.5,
-        ease: "none",
-        scrollTrigger: {
-          trigger: triggerElm,
-          start: "bottom bottom",
-          end: `+=${boxElm.clientWidth}`,
-          scrub: true,
-          pin: true,
-          pinSpacing: true,
-          invalidateOnRefresh: true,
-        },
-      });
-    }
-  }, [width]);
-
   return (
-    <Section heading="EVENT" subHeading="イベント" ref={triggerRef} id="event">
+    <Section heading="EVENT" subHeading="イベント" id="event">
       <div className="px-5 pl-4 md:ml-shead">
         <Paragraph>
           10/23.24の間にクラスターで開催されるイベントです！チェックしておこう
@@ -48,18 +14,19 @@ export const Event: VFC = () => {
           </span>
         </Paragraph>
       </div>
-      <div className="flex items-center h-[512px] relative overflow-x-hidden">
-        <div className="absolute flex pl-12 my-auto space-x-24 md:ml-shead" ref={boxRef}>
-          {new Array(8).fill(0).map((v, i) => (
-            <EventCard
-              key={i.toString()}
-              title={"開会式"}
-              description={"芸工展の幕開けを見届けよう！"}
-              imageSrc={"/images/event.png"}
-              size="md"
-            />
-          ))}
-        </div>
+      <div className="relative flex flex-col items-center justify-center overflow-x-hidden md:flex-row md:ml-shead">
+        <EventCard
+          title={"開会式"}
+          description={"芸工展の幕開けを見届けよう！"}
+          imageSrc={"/images/event.png"}
+          size="md"
+        />
+        <EventCard
+          title={"閉会式"}
+          description={"芸工祭も閉幕です！みんなで見届けよう"}
+          imageSrc={"/images/event.png"}
+          size="md"
+        />
       </div>
     </Section>
   );
