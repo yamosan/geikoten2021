5ad37007484be8a45bb97d069b6a3065a9f44e75
yamori-masato <yamorimasato@gmail.com>
2021-10-09 03:16:15 +0900

    画像最適化APIにimgixを指定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  images: {
+    loader: "imgix",
+    path: "/",
+  },
   future: { strictPostcssConfiguration: true },
   reactStrictMode: true,
   trailingSlash: isProd,
