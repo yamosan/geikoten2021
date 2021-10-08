3488ff6ee377f8d9d91f00e7dbfc0e21d9325b88
yamori-masato <yamorimasato@gmail.com>
2021-10-09 02:37:51 +0900

    本番環境用の設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-const nextConfig = {
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
   future: { strictPostcssConfiguration: true },
   reactStrictMode: true,
+  trailingSlash: isProd,
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
 };
-
-module.exports = nextConfig;
