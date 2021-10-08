3fd05d24e74b28a177514852afa0d44e57b26c12
yamori-masato <yamorimasato@gmail.com>
2021-10-09 00:39:30 +0900

    Projectデータを追加

---
+++ src/models/Project.ts
@@ -0,0 +1,10 @@
+export type Project = {
+  id: number;
+  title: string;
+  subTitle: string;
+  description: string;
+  // images: {
+  //   thumbnailUrl: string;
+  //   backgroundUrl: string;
+  // };
+};
