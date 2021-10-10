76312afa7638149fd9f7f1916201b6156b8cb7bb
yamori-masato <yamorimasato@gmail.com>
2021-10-11 04:34:34 +0900

    WebContentモデルを追加

---
+++ src/models/WebContent.ts
@@ -0,0 +1,16 @@
+type ContentType = "website" | "game" | "virtual";
+
+type Author = {
+  name: string;
+  grade: 1 | 2 | 3;
+  class: "M" | "N" | "O";
+};
+
+export type WebContent = {
+  id: number;
+  type: ContentType;
+  title: string;
+  authors: Author[];
+  thumbnailUrl: string;
+  siteUrl: string;
+};
