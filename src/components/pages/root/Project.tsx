5eed3cc8bfecfdf2bdf0ce513f8c1ab9f32f44c6
yamori-masato <yamorimasato@gmail.com>
2021-10-11 03:50:16 +0900

    fix

--- src/components/pages/root/Project.tsx
+++ src/components/pages/root/Project.tsx
@@ -57,7 +57,7 @@ export const Project: VFC<Props> = ({ projects }) => {
       </div>
       <div className="flex items-center h-[66vh] bg-lightGray relative overflow-x-hidden">
         <Image src="/images/top_bg.jpg" alt="背景" layout="fill" objectFit="cover" />
-        <div className="absolute flex pl-12 my-auto space-x-24 h-3/5 max-h-[800px] md:ml-shead" ref={boxRef}>
+        <div className="absolute flex pl-12 my-auto space-x-24 h-[65%] max-h-[800px] md:ml-shead" ref={boxRef}>
           {projects.map((project, i) => (
             <Link href={project.siteUrl.href} key={project.id.toString()}>
               <a className="block h-full">
