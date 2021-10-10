cb9d42510ff4123beddc0f2e4b57ae3d59d0cd28
yamori-masato <yamorimasato@gmail.com>
2021-10-10 13:21:39 +0900

    next/imageの最適化処理を無効にする

--- src/components/basics/Image.tsx
+++ src/components/basics/Image.tsx
@@ -3,12 +3,12 @@ import type { ComponentPropsWithoutRef } from "react";
 
 import { resolvePath } from "@/utils/resolvePath";
 
-type Props = ComponentPropsWithoutRef<typeof NextImage> & { src: string }; // src: string | StaticImport -> string
+type Props = Omit<ComponentPropsWithoutRef<typeof NextImage>, "quality"> & { src: string }; // src: string | StaticImport -> string
 
 const customLoader = ({ src }) => src;
 
 // TODO: next-optimized-imageを使ってビルド時に画像最適化
 export const Image = (props: Props) => {
   const url = props.src?.startsWith("/") ? resolvePath(props.src) : props.src;
-  return <NextImage {...props} src={url} loader={customLoader} />;
+  return <NextImage {...props} src={url} loader={customLoader} unoptimized />;
 };
