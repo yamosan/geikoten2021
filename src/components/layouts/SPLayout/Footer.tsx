e6785812e27532b2c97394a9b8573eb810f7fb2b
yamori-masato <yamorimasato@gmail.com>
2021-10-12 02:15:00 +0900

    footerのスタイルを調整

--- src/components/layouts/SPLayout/Footer.tsx
+++ src/components/layouts/SPLayout/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: VFC = () => {
   const HomepageLink = () => (
     <ExternalLink href={HOMEPAGE_LINK}>
       <div className="flex space-x-0.5">
-        <p className="text-sm leading-none underline">名古屋市立大学芸術工学部HP</p>
+        <p className="text-xs leading-none underline">名古屋市立大学芸術工学部HP</p>
         <ExternalLinkIcon />
       </div>
     </ExternalLink>
@@ -19,22 +19,22 @@ export const Footer: VFC = () => {
     <footer className="relative bg-lightBrown">
       <ScrollToTop className="absolute top-0 right-0" />
 
-      <div className="flex flex-col items-center py-16 text-white">
+      <div className="flex flex-col px-8 pt-16 pb-8 text-white">
         <div className="flex flex-col">
           <div className="flex flex-col items-start space-y-3">
-            <p className="text-lg font-bold leading-none">名古屋市立大学芸術工学部</p>
+            <p className="text-sm font-bold leading-none">名古屋市立大学芸術工学部</p>
             <HomepageLink />
           </div>
           <div className="mt-5">
-            <p className="text-sm leading-loose">
+            <p className="text-xs leading-loose">
               〒464-008 名古屋市千種区北千種2丁目1番10号
               <br />
               TEL052-721-1225(代) FAX052-721-3110
             </p>
           </div>
-          <div className="mt-6 text-center">
+          <div className="mt-10 text-center">
             {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
-            <small className="text-white">©️ Copyright 2021geikoten</small>
+            <small className="text-white text-xxs">©️ Copyright 2021geikoten</small>
           </div>
         </div>
       </div>
