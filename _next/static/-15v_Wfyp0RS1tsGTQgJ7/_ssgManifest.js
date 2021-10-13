1fe29f936b2691dc8f247a8a6b3374aef8a50d3b
yamosan <yamosan@users.noreply.github.com>
2021-10-13 04:27:50 +0000

    deploy: 58cdbd28719bc7b068a340e936b6b064220e36d2

---
+++ _next/static/-15v_Wfyp0RS1tsGTQgJ7/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fproject\u002Fcm","\u002Fproject\u002Fgame","\u002Fproject\u002Fstage","\u002Fproject\u002Fweb"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
