e1862b5dc38963f395410eca60c06e6ea056c366
yamori-masato <yamorimasato@gmail.com>
2021-10-13 13:26:25 +0900

    project/gameを追加

--- src/components/pages/project/WebContents.tsx
+++ src/components/pages/project/WebContents.tsx
@@ -16,67 +16,65 @@ export const WebContents: VFC<Props> = ({ groupLabel, contents, buttonLabel }) =
   const isWide = useMedia("(min-width: 1024px)"); // TODO: tailwind.config.jsから取得
 
   return (
-    <div className="flex flex-col space-y-24 pt-10 md:pt-24 pb-28 md:pb-36">
-      <section>
-        <h2 className="text-text text-center py-2 lg:py-4 bg-white lg:max-w-[800px] mx-auto w-11/12 lg:w-8/12 border-2 border-brown">
-          {groupLabel}
-        </h2>
-        <div className="mt-9 lg:mt-16 lg:px-4 lg:max-w-[1100px] mx-auto">
-          {contents.map((item, i) => (
-            <div key={item.id}>
-              {i !== 0 && <hr className="border-none bg-gray h-px my-10 lg:my-20" />}
-              <div
-                className={clsx(
-                  "flex flex-col space-x-0 lg:space-x-5 w-11/12 mx-auto lg:w-full lg:justify-between",
-                  i % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+    <section className="pt-10 md:pt-24 pb-28 md:pb-36">
+      <h2 className="text-text text-center py-2 lg:py-4 bg-white lg:max-w-[800px] mx-auto w-11/12 lg:w-8/12 border-2 border-brown">
+        {groupLabel}
+      </h2>
+      <div className="mt-9 lg:mt-16 lg:px-4 lg:max-w-[1100px] mx-auto">
+        {contents.map((item, i) => (
+          <div key={item.id}>
+            {i !== 0 && <hr className="border-none bg-gray h-px my-10 lg:my-20" />}
+            <div
+              className={clsx(
+                "flex flex-col space-x-0 lg:space-x-5 w-11/12 mx-auto lg:w-full lg:justify-between",
+                i % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+              )}
+            >
+              <div className="flex-1 relative w-full rounded-xl overflow-hidden">
+                {isWide ? (
+                  <Image src={item.thumbnailUrl} alt={item.title} layout="fill" objectFit="contain" />
+                ) : (
+                  <Image width={600} height={345} src={item.thumbnailUrl} alt={item.title} layout="responsive" />
                 )}
-              >
-                <div className="flex-1 relative w-full rounded-xl overflow-hidden">
-                  {isWide ? (
-                    <Image src={item.thumbnailUrl} alt={item.title} layout="fill" objectFit="contain" />
-                  ) : (
-                    <Image width={600} height={345} src={item.thumbnailUrl} alt={item.title} layout="responsive" />
-                  )}
-                </div>
+              </div>
 
-                <div className="flex flex-col space-y-8 w-full lg:space-y-5 lg:justify-between lg:w-[392px] lg:h-full lg:min-h-[332px]">
-                  <div className="flex flex-col w-full text-text">
-                    <h3 className="font-bold text-2xl mt-4 lg:mt-3 whitespace-pre-wrap">{item.title}</h3>
-                    <div className="flex items-center space-x-3 mt-4 lg:mt-6">
-                      <div className="flex space-x-1">
-                        {item.authors.map((author) => (
-                          <div
-                            key={author.name}
-                            className="relative rounded-full overflow-hidden w-[44px] h-[44px] lg:w-[52px] lg:h-[52px]"
-                          >
-                            <Image src={author.avatarUrl} alt={item.title} layout="fill" objectFit="cover" />
-                          </div>
-                        ))}
-                      </div>
-                      <ul>
-                        {item.authors.map((author, i, ary) => (
-                          <li key={author.name} className="inline-block text-sm">
-                            {author.grade}
-                            {author.class}
-                            {author.name}
-                            {i !== ary.length - 1 && "・"}
-                          </li>
-                        ))}
-                      </ul>
+              <div className="flex flex-col space-y-8 w-full lg:space-y-5 lg:justify-between lg:w-[392px] lg:h-full lg:min-h-[332px]">
+                <div className="flex flex-col w-full text-text">
+                  <h3 className="font-bold text-2xl mt-4 lg:mt-3 whitespace-pre-wrap">{item.title}</h3>
+                  <div className="flex items-center space-x-3 mt-4 lg:mt-6">
+                    <div className="flex space-x-1">
+                      {item.authors.map((author) => (
+                        <div
+                          key={author.name}
+                          className="relative rounded-full overflow-hidden w-[44px] h-[44px] lg:w-[52px] lg:h-[52px]"
+                        >
+                          <Image src={author.avatarUrl} alt={item.title} layout="fill" objectFit="cover" />
+                        </div>
+                      ))}
                     </div>
-                    <p className="mt-4 lg:mt-3 text-base leading-relaxed whitespace-pre-wrap">{item.description}</p>
+                    <ul>
+                      {item.authors.map((author, i, ary) => (
+                        <li key={author.name} className="inline-block text-sm">
+                          {author.grade}
+                          {author.class}
+                          {author.name}
+                          {i !== ary.length - 1 && "・"}
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                  <div className="flex justify-center">
-                    <LinkButton direction="right" href={item.siteUrl}>
-                      {buttonLabel}
-                    </LinkButton>
-                  </div>
+                  <p className="mt-4 lg:mt-3 text-base leading-relaxed whitespace-pre-wrap">{item.description}</p>
+                </div>
+                <div className="flex justify-center">
+                  <LinkButton direction="right" href={item.siteUrl}>
+                    {buttonLabel}
+                  </LinkButton>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-      </section>
-    </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
---
+++ src/pages/project/game.tsx
@@ -0,0 +1,49 @@
+import type { GetStaticProps, NextPage } from "next";
+
+import { Layout } from "@/components/layouts/Layout";
+import { Back } from "@/components/pages/project/Back";
+import { Hero } from "@/components/pages/project/Hero";
+import { WebContents } from "@/components/pages/project/WebContents";
+import { Project, WebContent } from "@/models";
+import { findProject } from "@/utils/getProjects";
+import { getWebContents } from "@/utils/getWebContents";
+
+type Props = {
+  project: Project;
+  webContents: {
+    game: WebContent[];
+    virtual: WebContent[];
+  };
+};
+
+const PROJECT_NAME = "game";
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const project = await findProject((project) => project.name === PROJECT_NAME);
+  const webContents = {
+    game: getWebContents({ type: "game" }),
+    virtual: getWebContents({ type: "virtual" }),
+  };
+
+  return {
+    props: {
+      project,
+      webContents,
+    },
+  };
+};
+
+const Game: NextPage<Props> = ({ project, webContents }) => {
+  return (
+    <Layout>
+      <Hero project={project} />
+      <main className="w-full md:pl-shead flex flex-col space-y-[212px] lg:space-y-[120px] items-stretch bg-white">
+        <WebContents contents={webContents.game} groupLabel="ゲーム" buttonLabel="ゲームをプレイ" />
+        <WebContents contents={webContents.game} groupLabel="バーチャル会場" buttonLabel="clusterに行く" />
+      </main>
+      <Back />
+    </Layout>
+  );
+};
+
+export default Game;
