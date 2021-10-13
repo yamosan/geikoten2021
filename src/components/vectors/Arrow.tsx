b1debba1b4cc35f672f2dcc9508fc23a3684dc29
yamori-masato <yamorimasato@gmail.com>
2021-10-13 21:55:17 +0900

    fix

--- src/components/vectors/Arrow.tsx
+++ src/components/vectors/Arrow.tsx
@@ -2,10 +2,10 @@ import clsx from "clsx";
 import React, { VFC } from "react";
 
 type Props = {
-  className: string;
+  className?: string;
   round?: boolean;
   strokeWidth?: string | number;
-  stroke?: "white" | "lightBrown";
+  stroke?: "white" | "black";
   direction?: "top" | "bottom" | "left" | "right";
 };
 
@@ -24,6 +24,10 @@ export const Arrow: VFC<Props> = ({
       "-rotate-90": direction === "left",
       "rotate-90": direction === "right",
     },
+    {
+      "text-white": stroke === "white",
+      "text-lightBrown": stroke === "black",
+    },
     className
   );
 
@@ -31,7 +35,7 @@ export const Arrow: VFC<Props> = ({
     <svg className={styles} viewBox="0 0 29 19" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M27 18L14.5 2L2 18"
-        stroke={stroke}
+        stroke="currentColor"
         strokeWidth={strokeWidth}
         strokeLinejoin="round"
         strokeLinecap={round ? "round" : undefined}
