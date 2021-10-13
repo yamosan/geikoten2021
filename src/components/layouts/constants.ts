b392d068ea6edf3d05c169145ae19c68d86cfe1b
yamori-masato <yamorimasato@gmail.com>
2021-10-13 22:00:59 +0900

    fix

--- src/components/layouts/constants.ts
+++ src/components/layouts/constants.ts
@@ -9,6 +9,12 @@ export type TLink = {
 export const NAV_LINKS: TLink[] = [
   { id: "top", text: "TOP", subText: "トップ", path: "/#top", activePatterns: ["/#top", "/"] },
   { id: "about", text: "ABOUT", subText: "芸工展とは", path: "/#about", activePatterns: ["/#about", "/about"] },
-  { id: "project", text: "PROJECT", subText: "企画", path: "/#project", activePatterns: ["/#project", "/project/*"] },
+  {
+    id: "project",
+    text: "PROJECT",
+    subText: "企画",
+    path: "/#project",
+    activePatterns: ["/#project", "/project/**/*"],
+  },
   { id: "event", text: "EVENT", subText: "イベント", path: "/#event", activePatterns: ["/#event"] },
 ];
