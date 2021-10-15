141e1e33fe0a633651542082ba8e6fbe0228fe82
yamori-masato <yamorimasato@gmail.com>
2021-10-16 02:12:46 +0900

    fix

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -5,4 +5,4 @@ export const INSTAGRAM_URL = "https://instagram.com/geikoten2021";
 export const HOMEPAGE_LINK = "https://www.nagoya-cu.ac.jp/sda";
 
 export const WORKS_URL =
-  "https://script.googleusercontent.com/macros/echo?user_content_key=xBTXeqiFN4Fx34XxGBX-pVATfpoGMixq6FqmVdLATEOH4NzLB8ysJIUjCKnveC5FWFi8Eg_PZQpnx9coLJi3_xqFH88mkNS-m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKcPQaKd6__WCL7VZlpWpn5bk5NC7GrRbWmA7DAHpJx3OgCT_JuK6Upfp9OsAFJaUIHv-4zZXcZljWjsLDDAuGnGvX6EzKix2A&lib=MPwhN9iUafJf3SngY242eL2CN3Tsxosd8";
+  "https://script.google.com/macros/s/AKfycbxvA5Va2kkLXO8Zcs4BsVi-hGWawMlIlZsnrgahGMUl1cxvNW188OO88hIbsh4RLRg/exec";
