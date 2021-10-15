57ee5416618f7e27f85b5ae4dec2d8bcc0e1ee9e
yamori-masato <yamorimasato@gmail.com>
2021-10-16 04:03:10 +0900

    Footerを修正

--- src/components/pages/project/exhibition/Footer.tsx
+++ src/components/pages/project/exhibition/Footer.tsx
@@ -3,6 +3,7 @@ import type { VFC } from "react";
 
 import { Arrow } from "@/components/vectors";
 import { THEMES } from "@/constants/exhibition";
+import useMedia from "@/hooks/useMediaQuery";
 import { Work } from "@/models";
 
 const getPagination = (currentTheme: Work["theme"]) => {
@@ -19,30 +20,55 @@ type Props = {
 
 export const Footer: VFC<Props> = ({ currentTheme }) => {
   const { prev, next } = getPagination(currentTheme);
+  const isWide = useMedia("(min-width: 768px)"); // TODO: tailwind.config.jsから取得
 
   return (
-    <div className="w-full md:pl-shead h-[165px] md:h-[245px]">
-      <div className="h-full flex items-center justify-between max-w-[810px] mx-auto px-4">
-        <Link href={prev.href}>
-          <a>
-            <div className="flex items-center space-x-1">
-              <Arrow round direction="left" stroke="black" className="w-[15px] mt-1.5" />
-              <div className="text-text border-b border-brown">{prev.theme}</div>
-            </div>
-          </a>
-        </Link>
-        <Link href="/#project">
-          <a className="text-text border-b border-brown">部屋選択に戻る</a>
-        </Link>
-        <Link href={next.href}>
-          <a>
-            <div className="flex items-center space-x-1">
-              <div className="text-text border-b border-brown">{next.theme}</div>
-              <Arrow round direction="right" stroke="black" className="w-[15px] mt-1.5" />
-            </div>
-          </a>
-        </Link>
-      </div>
-    </div>
+    <>
+      {isWide ? (
+        <div className="w-full md:pl-shead h-[165px] md:h-[245px]">
+          <div className="h-full flex items-center justify-between max-w-[810px] mx-auto px-4">
+            <Prev href={prev.href} theme={prev.theme} />
+            <Link href="/#project">
+              <a className="text-text border-b border-brown">部屋選択に戻る</a>
+            </Link>
+            <Next href={next.href} theme={next.theme} />
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col space-y-6 justify-center items-center w-full md:pl-shead h-[196px]">
+          <div className="flex justify-center">
+            <Link href="/#project">
+              <a className="text-text border-b border-brown">部屋選択に戻る</a>
+            </Link>
+          </div>
+          <div className="flex items-center justify-between w-full mx-auto px-4">
+            <Prev href={prev.href} theme={prev.theme} />
+            <Next href={next.href} theme={next.theme} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
+
+const Prev: VFC<{ href: string; theme: Work["theme"] }> = ({ href, theme }) => (
+  <Link href={href}>
+    <a>
+      <div className="flex items-center space-x-1">
+        <Arrow round direction="left" stroke="black" className="w-[15px] mt-1.5" />
+        <div className="text-text border-b border-brown">{theme}</div>
+      </div>
+    </a>
+  </Link>
+);
+
+const Next: VFC<{ href: string; theme: Work["theme"] }> = ({ href, theme }) => (
+  <Link href={href}>
+    <a>
+      <div className="flex items-center space-x-1">
+        <div className="text-text border-b border-brown">{theme}</div>
+        <Arrow round direction="right" stroke="black" className="w-[15px] mt-1.5" />
+      </div>
+    </a>
+  </Link>
+);
