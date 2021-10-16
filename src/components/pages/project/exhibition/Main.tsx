2990f82f49970184cbd6e670b49f70547b2e3b77
yamori-masato <yamorimasato@gmail.com>
2021-10-16 13:11:45 +0900

    WorkCardコンポーネントのクリック時の挙動を実装

--- src/components/pages/project/exhibition/Main.tsx
+++ src/components/pages/project/exhibition/Main.tsx
@@ -1,7 +1,9 @@
 import type { VFC } from "react";
 
-import { Section, WorkCard } from "@/components/ui";
+import { Section } from "@/components/ui";
+import { WorkCard } from "@/components/ui/WorkCard";
 import { THEMES } from "@/constants/exhibition";
+import useMedia from "@/hooks/useMediaQuery";
 import { Work } from "@/models";
 type Props = {
   works: Work[];
@@ -12,19 +14,19 @@ export const Main: VFC<Props> = ({ works, theme }) => {
   const target = THEMES.find((t) => t.theme === theme);
   const colorKey = target.id;
 
+  const isWide = useMedia("(min-width: 768px)");
+
   return (
-    <main>
-      <Section heading="WORKS" subHeading="作品" headerColor={colorKey}>
-        <div className="bg-white md:pl-shead pt-3">
-          <div className="w-11/12 mx-auto lg:max-w-app ">
-            <div className="grid gap-x-8 gap-y-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-full">
-              {works.map((work) => (
-                <WorkCard key={work.id} work={work} />
-              ))}
-            </div>
+    <Section heading="WORKS" subHeading="作品" headerColor={colorKey}>
+      <div className="bg-white md:pl-shead pt-3">
+        <div className="w-11/12 mx-auto lg:max-w-app ">
+          <div className="grid gap-x-8 gap-y-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:w-full">
+            {works.map((work) => (
+              <WorkCard key={work.id} work={work} dialogLayout={isWide ? "row" : "column"} />
+            ))}
           </div>
         </div>
-      </Section>
-    </main>
+      </div>
+    </Section>
   );
 };
--- src/components/ui/WorkCard.tsx
+++ src/components/ui/WorkCard.tsx
@@ -1,23 +1,32 @@
-import React, { ComponentPropsWithoutRef, FC } from "react";
+import React, { ComponentProps, ComponentPropsWithoutRef, FC, useState } from "react";
 
 import { Image } from "@/components/basics";
 import { Work } from "@/models";
 
 import { VoteButton } from "./VoteButton";
+import { WorkDialog } from "./WorkDialog";
 
 type Props = {
   work: Work;
+  dialogLayout: ComponentProps<typeof WorkDialog>["layout"];
 } & ComponentPropsWithoutRef<"div">;
 
-export const WorkCard: FC<Props> = ({ work }) => {
+export const WorkCard: FC<Props> = ({ work, dialogLayout }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
+      <WorkDialog open={open} onClose={() => setOpen(false)} work={work} layout={dialogLayout} />
+
       <div className="flex flex-col">
-        <figure className="relative w-full border-lightBrown border-2 drop-shadow">
-          <Image src={work.thumbnailUrl} layout="responsive" width={283} height={213} alt={work.title} />
-        </figure>
+        <button onClick={() => setOpen(true)}>
+          <figure className="relative w-full border-lightBrown border-2 drop-shadow">
+            <Image src={work.thumbnailUrl} layout="responsive" width={283} height={213} alt={work.title} />
+          </figure>
+        </button>
+
         <div className="flex flex-col">
-          <p className="text-text text-sm pt-3 pb-2 truncate">{work.description}</p>
+          <p className="text-text text-sm pt-3 pb-2 truncate">{work.title}</p>
           <div className="flex items-center justify-between w-full">
             <div className="flex items-center space-x-2">
               <div className="relative rounded-full overflow-hidden flex justify-center items-center">
