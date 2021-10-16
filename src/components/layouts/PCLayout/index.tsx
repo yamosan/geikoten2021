794a1ab84955ffe9f12b418e903a9facd5b75df9
yamori-masato <yamorimasato@gmail.com>
2021-10-16 16:31:04 +0900

    fix

--- src/components/layouts/PCLayout/index.tsx
+++ src/components/layouts/PCLayout/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const PCLayout: VFC<Props> = ({ children }) => {
   return (
     <div className="flex">
-      <Header className="fixed top-0 left-0 z-10 h-screen bg-menuBg w-shead"></Header>
+      <Header className="fixed top-0 left-0 z-50 h-screen bg-menuBg w-shead"></Header>
       <div className="flex flex-col justify-between w-full min-h-screen">
         <main className="bg-bg">{children}</main>
         <Footer />
