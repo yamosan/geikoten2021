3567fe4a010e40b8e22d5c7c0b4e46872127a86b
yamori-masato <yamorimasato@gmail.com>
2021-10-18 01:07:46 +0900

    「ボタンを押して、好きな作品に投票しよう！」を追加

--- src/components/pages/project/exhibition/RoomSelection.tsx
+++ src/components/pages/project/exhibition/RoomSelection.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import type { VFC } from "react";
 
 import { Image } from "@/components/basics";
-import { LinkButton, RoomInfoCard } from "@/components/ui";
+import { LinkButton, RoomInfoCard, VoteButton } from "@/components/ui";
 import useMedia from "@/hooks/useMediaQuery";
 import { Room } from "@/models";
 
@@ -11,35 +11,75 @@ type Props = {
 };
 
 export const RoomSelection: VFC<Props> = ({ rooms }) => {
+  // TODO: 配列で取得
   const isXWide = useMedia("(min-width: 1024px)");
+  const isMd = useMedia("(min-width: 640px)");
 
   return (
-    <main className="bg-white md:pl-shead">
-      {/* TODO:  */}
-
-      <div className="w-11/12 lg:px-4 lg:max-w-[845px] mx-auto">
-        <div className={clsx("flex flex-col space-y-16 justify-center py-24")}>
-          {rooms.map((room, i) => (
-            <div key={room.id}>
-              <RoomInfoCard
-                room={room}
-                layout={isXWide ? (i % 2 === 0 ? "row" : "row-reverse") : "column"}
-                showLink
-                className="min-h-[265px]"
-              />
-            </div>
-          ))}
+    <>
+      <main className="bg-white md:pl-shead pt-14 pb-20">
+        <div className="flex flex-col space-y-2 items-center px-10 w-max max-w-full mx-auto">
+          <div
+            className={clsx(
+              "flex items-start md:items-center space-x-1.5 pb-2 md:px-3 border-b border-lightBrown border-dashed",
+              "hachinoji"
+            )}
+          >
+            <VoteButton disabled className="flex-shrink-0" />
+            <h4 className="font-bold text-base pt-1 md:pt-0 sm">
+              {/* inline-block × w-max がしたい。(ボックスモデル的に無理) */}
+              ボタンを押して、
+              {!isMd && <br />}
+              好きな作品に投票しよう！
+            </h4>
+          </div>
+          <p className="self-end">
+            <time>
+              【期間:<time dateTime="2021-10-23">2021/10/23</time>~<time dateTime="2021-10-24">24</time>】
+            </time>
+          </p>
         </div>
-      </div>
-      <hr className="border-none bg-gray h-px my-8 lg:mt-30 lg:mb-16" />
-      <div className="w-11/12 lg:px-4 lg:max-w-[845px] mx-auto pb-20">
-        <ClusterCard />
-      </div>
-    </main>
+
+        <div className="mt-12 w-11/12 lg:px-4 lg:max-w-[845px] mx-auto">
+          <div className={clsx("flex flex-col space-y-16 justify-center pb-24")}>
+            {rooms.map((room, i) => (
+              <div key={room.id}>
+                <RoomInfoCard
+                  room={room}
+                  layout={isXWide ? (i % 2 === 0 ? "row" : "row-reverse") : "column"}
+                  showLink
+                  className="min-h-[265px]"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <hr className="border-none bg-gray h-px my-8 lg:mt-30 lg:mb-16" />
+        <div className="w-11/12 lg:px-4 lg:max-w-[845px] mx-auto">
+          <ClusterCard />
+        </div>
+      </main>
+
+      <style jsx>{`
+        .hachinoji {
+          @apply relative;
+        }
+        .hachinoji::before {
+          @apply absolute bottom-1/2 left-0 h-4/5 border-l-[3px] transform translate-y-1/2 -translate-x-4 rotate-[-30deg] origin-bottom-left
+                  sm:-rotate-45;
+          content: "";
+        }
+        .hachinoji::after {
+          @apply absolute bottom-1/2 right-0 h-4/5 border-r-[3px] transform translate-y-1/2 translate-x-4 rotate-[30deg] origin-bottom-right
+                  sm:rotate-45;
+          content: "";
+        }
+      `}</style>
+    </>
   );
 };
 
-const ClusterCard = () => {
+const ClusterCard: VFC = () => {
   return (
     <div className={clsx("flex flex-col space-y-2 lg:flex-row lg:space-x-6 items-center")}>
       <div className="flex-[5.8] h-full w-full relative rounded-2xl overflow-hidden">
