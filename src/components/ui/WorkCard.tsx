058ec94bcc0e57ac940704027c31d5c252017ad7
yamori-masato <yamorimasato@gmail.com>
2021-10-17 19:34:28 +0900

    fix

--- src/components/ui/WorkCard.tsx
+++ src/components/ui/WorkCard.tsx
@@ -57,7 +57,7 @@ export const WorkCard: FC<Props> = ({ work, dialogLayout, voted = false, handleC
                     alt=""
                     priority
                   />
-                  <div className="absolute z-10 transform top-1/2 left-1/2 -translate-1/2">
+                  <div className="absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <Loading className="animate-spin" />
                   </div>
                 </div>
@@ -70,7 +70,7 @@ export const WorkCard: FC<Props> = ({ work, dialogLayout, voted = false, handleC
           <p className="text-text text-sm pt-3 pb-2 truncate">{work.title}</p>
           <div className="flex items-center justify-between space-x-1 w-full">
             <div className="flex items-center space-x-2">
-              <div className="relative rounded-full overflow-hidden flex justify-center items-center">
+              <div className="relative rounded-full overflow-hidden flex justify-center items-center bg-lightGray">
                 <Image src={work.author?.avatarUrl} width={38} height={38} alt={work.author.name} objectFit="cover" />
               </div>
               <p className="text-text text-[13px] font-regular">
