1f8ab9888265aff8d98f0d4274a09a8544d00253
yamori-masato <yamorimasato@gmail.com>
2021-10-20 05:36:23 +0900

    MenuButtonコンポーネントを追加

---
+++ src/components/ui/MenuButton.tsx
@@ -0,0 +1,46 @@
+import clsx from "clsx";
+import type { ComponentPropsWithoutRef } from "react";
+import React, { forwardRef } from "react";
+
+type Props = {
+  active?: boolean;
+} & ComponentPropsWithoutRef<"button">;
+
+export const MenuButton = forwardRef<HTMLButtonElement, Props>((props, ref) => {
+  const { active, className, ...attrs } = props;
+
+  return (
+    <>
+      <button
+        className={clsx(
+          "min-w-[100px] text-sm py-2 px-4 rounded-md border-lightBrown inline-flex justify-between space-x-1 items-center hover:text-opacity-100",
+          {
+            "text-white bg-lightBrown": active,
+            "text-lightBrown bg-white": !active,
+          },
+          className
+        )}
+        ref={ref}
+        {...attrs}
+      >
+        <span>ジャンル</span>
+        <span className="check" aria-hidden />
+      </button>
+
+      <style jsx>{`
+        .check {
+          @apply w-[8px] h-[12px] border-t-[6px] border-l-[4px] border-r-[4px] border-current transform translate-y-1/4;
+          border-left-color: transparent;
+          border-right-color: transparent;
+          border-bottom-color: transparent;
+        }
+      `}</style>
+    </>
+  );
+});
+
+MenuButton.defaultProps = {
+  active: false,
+};
+
+MenuButton.displayName = "MenuButton";
