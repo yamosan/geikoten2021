b699b05958b3f7590a41e4d30856393408b9c920
yamori-masato <yamorimasato@gmail.com>
2021-10-20 14:23:57 +0900

    fix

--- src/components/basics/Checkbox.tsx
+++ src/components/basics/Checkbox.tsx
@@ -11,7 +11,7 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
 
   return (
     <>
-      <label className={clsx("inline-flex align-middle cursor-pointer select-none", "text-text text-sm", className)}>
+      <label className={clsx("inline-flex items-center cursor-pointer select-none", "text-text text-sm", className)}>
         <input className="sr-only" type="checkbox" ref={ref} {...attrs} />
         <span
           className="relative mr-2 w-5 h-5 border border-gray rounded-sm inline-flex items-center justify-center"
@@ -19,15 +19,17 @@ export const Checkbox = forwardRef<HTMLInputElement, Props>((props, ref) => {
           aria-hidden
           aria-checked={checked}
         >
-          <svg width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M1.5 5.5L6 10L13.5 1"
-              stroke="#504746"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+          <span className={clsx("transition-opacity", checked ? "opacity-100" : "opacity-0")}>
+            <svg width="15" height="11" viewBox="0 0 15 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M1.5 5.5L6 10L13.5 1"
+                stroke="#504746"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
         </span>
         {label}
       </label>
