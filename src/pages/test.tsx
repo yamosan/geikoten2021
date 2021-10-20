2e0684e0dd275a67f02f3f060ebab5bb97bc09a6
yamori-masato <yamorimasato@gmail.com>
2021-10-20 20:40:27 +0900

    fix

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 
-import { Checkbox } from "@/components/basics";
 import { DropdownCheckboxes } from "@/components/ui";
 
 const GENRE = [
@@ -11,12 +10,23 @@ const GENRE = [
   { label: "Web", value: "Web" },
   { label: "その他", value: "その他" },
 ];
+const GRADE = [
+  { label: "1年", value: "1" },
+  { label: "2年", value: "2" },
+  { label: "3年", value: "3" },
+];
+const CLASS = [
+  { label: "情報", value: "M" },
+  { label: "産業", value: "N" },
+  { label: "建築", value: "O" },
+];
 
 const Test: NextPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-lightGray">
       <DropdownCheckboxes items={GENRE} label="ジャンル" />
-      <Checkbox checked={true} onChange={() => {}} />
+      <DropdownCheckboxes items={GRADE} label="学年" />
+      <DropdownCheckboxes items={CLASS} label="学科" />
     </div>
   );
 };
