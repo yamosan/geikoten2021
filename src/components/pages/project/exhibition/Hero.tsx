9fa491a5e7efbcd283972abf7a78cee8b73f1884
yamori-masato <yamorimasato@gmail.com>
2021-10-23 07:31:18 +0900

    各部屋の背景を追加

--- src/components/pages/project/exhibition/Hero.tsx
+++ src/components/pages/project/exhibition/Hero.tsx
@@ -15,7 +15,13 @@ export const Hero: VFC<Props> = ({ room }) => {
   return (
     <div className="relative">
       <Div100vh className="relative">
-        <Image src="/images/work_bg.png" alt="背景" layout="fill" objectFit="cover" priority />
+        <Image
+          src={`/images/project/exhibition/${room.theme}/bg.png`}
+          alt="背景"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
         <div className="flex justify-center md:pl-shead h-full relative pt-thead md:pt-0 pb-[80px]">
           <div className="w-11/12 lg:w-5/6 h-full flex justify-center items-center lg:max-w-app lg:mx-auto">
             <RoomInfoCard room={room} layout={isXWide ? "row" : "column"} card />
