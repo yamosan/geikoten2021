4d36692f7f7aeaa9637acdfd9798d00861f31caa
yamori-masato <yamorimasato@gmail.com>
2021-10-23 10:07:28 +0900

    fix

--- src/components/pages/root/Top.tsx
+++ src/components/pages/root/Top.tsx
@@ -1,4 +1,5 @@
-import type { VFC } from "react";
+import clsx from "clsx";
+import { useEffect, useState, VFC } from "react";
 
 import { Div100vh, Image } from "@/components/basics";
 import { ScrollDown, SponsorsCarousel, VisitorCounter } from "@/components/ui";
@@ -14,36 +15,87 @@ export const Top: VFC<Props> = ({ goldSponsors }) => {
   const isWide = useMedia("(min-width: 768px)");
   const { count } = useVisitorCount();
 
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
   return (
-    <div className="relative" id="top">
-      <Div100vh className="relative">
-        <Image src="/images/top_bg.jpg" alt="背景" layout="fill" objectFit="cover" priority />
+    <>
+      <div className="relative" id="top">
+        <Div100vh className="relative">
+          <Image src="/images/top/bg.jpg" alt="背景" layout="fill" objectFit="cover" priority />
 
-        <div className="relative w-full h-full pt-thead md:pt-14">
-          {/* アクセスカウンタ */}
-          {isWide ? (
-            <div className="flex justify-end pr-4">
-              <VisitorCounter count={count} size="lg" />
+          <div
+            className={clsx(
+              "main-visual",
+              "transition duration-400 delay-[2000] absolute inset-0 md:ml-shead h-full z-10 max-w-full overflow-hidden",
+              {
+                "opacity-100": ready,
+                "opacity-0": !ready,
+              }
+            )}
+          >
+            <div className="h-full flex items-center">
+              <div className="relative h-full flex items-center transform transition scale-150 sm:scale-100">
+                <Image
+                  src="/images/top/main_visual.png"
+                  alt="メインビジュアル"
+                  width={996}
+                  height={614}
+                  objectFit="contain"
+                  priority
+                />
+              </div>
             </div>
-          ) : (
-            <div className="flex justify-start pl-4">
-              <VisitorCounter count={count} size="sm" />
-            </div>
-          )}
-        </div>
-      </Div100vh>
+          </div>
 
-      {isWide ? (
-        <div className="flex flex-col items-center w-full mb-24 -mt-24 pl-shead">
-          <ScrollDown className="z-10" />
-          <SponsorsCarousel sponsors={goldSponsors} className="w-4/5" navigationPosition="rl" />
-        </div>
-      ) : (
-        <div className="relative flex flex-col items-center pb-8 mb-24 -mt-24 overflow-x-hidden md:overflow-x-auto">
-          <ScrollDown className="z-10" />
-          <SponsorsCarousel sponsors={goldSponsors} className="w-[150%]" navigationPosition="bottom" />
-        </div>
-      )}
-    </div>
+          <div className="relative w-full h-full pt-thead md:pt-14">
+            {/* アクセスカウンタ */}
+            {isWide ? (
+              <div className="flex justify-end pr-4">
+                <VisitorCounter count={count} size="lg" />
+              </div>
+            ) : (
+              <div className="flex justify-start pl-4">
+                <VisitorCounter count={count} size="sm" />
+              </div>
+            )}
+          </div>
+        </Div100vh>
+
+        {isWide ? (
+          <div className="flex flex-col items-center w-full mb-24 -mt-24 pl-shead">
+            <ScrollDown className="z-10" />
+            <SponsorsCarousel sponsors={goldSponsors} className="w-4/5" navigationPosition="rl" />
+          </div>
+        ) : (
+          <div className="relative flex flex-col items-center pb-8 mb-24 -mt-24 overflow-x-hidden md:overflow-x-auto">
+            <ScrollDown className="z-10" />
+            <SponsorsCarousel sponsors={goldSponsors} className="w-[150%]" navigationPosition="bottom" />
+          </div>
+        )}
+      </div>
+
+      <style jsx>{`
+        .main-visual {
+          animation: float 3s linear infinite;
+        }
+
+        @keyframes float {
+          0% {
+            transform: translateY(0);
+          }
+          33.33333% {
+            transform: translateY(-10px);
+          }
+          66.66667%,
+          100% {
+            transform: translateY(0);
+          }
+        }
+      `}</style>
+    </>
   );
 };
